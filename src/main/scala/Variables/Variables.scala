7399dfadb6fb223517acab9f105313847eef1305
“[Nic <[nicjcoxen@gmail.com]>
2024-01-06 21:55:48 +1300

    variables

--- src/main/scala/Variables/Variables.scala
+++ src/main/scala/Variables/Variables.scala
@@ -9,6 +9,14 @@ object Variables {
     private val age = 25
     private val time = 1.5
 
+  /* st1 and st2 are different strings.
+  In Scala, "var" makes a variable mutable, that means that the object or value assigned to a "var" can be replaced.
+  Regarding "val": val cannot be replaced, and it is immutable.
+   */
+
+    var st1 = "Hello Scala"
+    val st2: String = "Hi Scala"
+
     println("Hello " + name + " you are : " + age) // concatenation interpolation
 
     println(s"$name is $age years old") // s string interpolation, not type safe
@@ -17,4 +25,6 @@ object Variables {
 
     println(s"Hello\n everyone")
 
+    println(st1);
+    println(st2);
 }
