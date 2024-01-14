32ef6b51775d2523b7fa70bcbd82c69d088d5562
“[Nic <[nicjcoxen@gmail.com]>
2024-01-15 08:02:46 +1300

    stringFormats

--- src/main/scala/VariableFunctions/StringFormat.scala
+++ src/main/scala/VariableFunctions/StringFormat.scala
@@ -3,15 +3,31 @@ package VariableFunctions
 object StringFormat {
   def main(args: Array[String]) {
 
-    /* Creating format strings
+    val factor = 6f / 1.5f
+    println(factor)
 
-     %d is used for Integers and %f is used for floating-points or Doubles.
-     */
-    val factor = 35f / 1.5f
+    /* % d is used
+  for Integers and % f is used
+  for floating - points or Doubles.
+    */
 
-    print(f"The factor is $factor%.3f") // 3f will give 3 decimal points
+    val firstName: "Nic" = "Nic"
+    val lastName = "Coxen"
+    val age = 35
+    val formatted = "%s %s, age %d".format(firstName, lastName, age)
+    println(formatted)
+
+    // %s is a placeholder for a string, and %d is a placeholder for a decimal or integer.
+
+
+    val height = 1.81
+    println(f"I am $height%2.2f meters tall")
+
+    /*In the f interpolator, %2.2f is a format specifier, which formats the given floating
+   point number as a string that represents a floating point number with at least 2
+   characters width and 2 decimal places2.
+*/
+
+
   }
-
 }
-
-
